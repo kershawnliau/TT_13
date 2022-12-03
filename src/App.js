5f1bccf1a4d612321fd6885f1f039ef101dc7f12
kershawnliau <61313589+kershawnliau@users.noreply.github.com>
2022-12-03 15:05:33 +0800

    useeffect for bank account

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 import Home from "./components/home/home";
 import Login from "./components/login-register/login";
 import ProfilePage from "./components/profile/ProfilePage";
+import Transaction from "./components/transaction/home"
 
 function App() {
   return (
-    // <BrowserRouter>
-    //     <Routes>
-    //       <Route exact path="/" element={<Login />}/>
-    //       <Route />
-    //     </Routes>
-    // </BrowserRouter>
-    <ProfilePage />
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />}/>
+          <Route exact path="/transaction" element={<Transaction />}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
