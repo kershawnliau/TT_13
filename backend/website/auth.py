ffca9b11f819e4d4949cabc859f35ee1bd64fbaf
danielwse <danieltzy@hotmail.com>
2022-12-03 11:53:59 +0800

    add conn to mysql local

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, request
+from . import mysql
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM bankaccount")
+    results = cursor.fetchall()
+    print(results)
+
     return "<h1> hello world<h1>"
+    
 
