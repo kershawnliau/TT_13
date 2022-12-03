42a4a57324a272959ce1116d1565080317c16b44
MingYuan Chen <optionrocks@hotmail.com>
2022-12-03 14:09:44 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/home";
+import Login from "./components/login-register/login";
 
 function App() {
-  return <Home />
+  return (
+  <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />}/>
+        <Route />
+      </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
