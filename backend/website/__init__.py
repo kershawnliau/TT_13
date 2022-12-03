b2a912fc56dfd2610c160971be9b684613a606d9
danielwse <danieltzy@hotmail.com>
2022-12-03 11:21:46 +0800

    flask endpoints backbone

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -3,6 +3,11 @@ from os import path
 from flask_login import LoginManager
 from flaskext.mysql import MySQL
 
+
 def create_app():
     app = Flask(__name__)
+
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix='/')
+    
     return app
