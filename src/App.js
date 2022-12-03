d5fdce230c8325269314fd1048ac8388b483abff
Chong Jing Hong <congjinghong@gmail.com>
2022-12-03 14:25:34 +0800

    profile

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/home";
 import Login from "./components/login-register/login";
+import ProfilePage from "./components/profile/ProfilePage";
 
 function App() {
   return (
-  <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />}/>
-        <Route />
-      </Routes>
-  </BrowserRouter>
-  )
+    // <BrowserRouter>
+    //     <Routes>
+    //       <Route exact path="/" element={<Login />}/>
+    //       <Route />
+    //     </Routes>
+    // </BrowserRouter>
+    <ProfilePage />
+  );
 }
 
 export default App;
